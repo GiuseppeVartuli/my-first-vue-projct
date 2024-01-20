c7fdf6dfee9d2902bf96e3b6f262b77cc9146557
GiuseppeVartuli <giuseppevartuli89@gmail.com>
2024-01-20 12:56:28 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Giuseppe",
+      background: "purple",
+      movie: "",
+    };
+  },
+  methods: {
+    greetings() {
+      console.log("ciao", this.message);
+    },
+  },
+};
+</script>
+
+<template>
+  <h1 :class="background">{{ message }}</h1>
+  <button v-on:click="greetings">Cliccami</button>
+
+  <label for="movie">Movie</label>
+  <input type="text" id="movie" v-model="movie" />
+
+  <p>{{ movie }}</p>
+</template>
+
+<style></style>
